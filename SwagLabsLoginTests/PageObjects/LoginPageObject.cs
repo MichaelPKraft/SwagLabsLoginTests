97956e5d19ffc84f347847c1d042f9789fbc9806
Michael Kraft <mkraft@nwlink.com>
2021-11-25 18:34:11 -0500

    Swag Labs Login Test - initial version

---
+++ SwagLabsLoginTests/PageObjects/LoginPageObject.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SwagLabsLoginTest.PageObjects
+{
+    /// <summary>
+    /// Swag Labs Login Page Object
+    /// </summary>
+    public class LoginPageObject
+    {
+        //The URL of the Swag Labs login page
+        private const string SwagLabsLoginUrl = "https://www.saucedemo.com/";
+
+        //The Selenium web driver to automate the browser
+        private readonly IWebDriver _webDriver;
+
+        //The default wait time in seconds for wait.Until
+        //public const int DefaultWaitInSeconds = 5;
+
+        public LoginPageObject(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        //Find elements
+        private IWebElement LoginLogo => _webDriver.FindElement(By.ClassName("login_logo"));
+        private IWebElement UserNameElement => _webDriver.FindElement(By.Id("user-name"));
+        private IWebElement PasswordElement => _webDriver.FindElement(By.Id("password"));
+        private IWebElement LoginButtonElement => _webDriver.FindElement(By.Id("login-button"));
+
+        private IWebElement ErrorTextElement => _webDriver.FindElement(By.XPath("//*[@data-test='error']"));
+
+        public bool LoginPageDisplayed()
+        {
+            return LoginLogo.Displayed;
+        }
+
+        public void EnterUserName(string userName)
+        {
+            UserNameElement.Clear();
+            UserNameElement.SendKeys(userName);
+        }
+
+        public void EnterPassword(string password)
+        {
+            PasswordElement.Clear();
+            PasswordElement.SendKeys(password);
+        }
+
+        public void ClickLoginButton()
+        {
+            LoginButtonElement.Click();
+        }
+
+        public string CheckForLoginError()
+        {
+            return ErrorTextElement.Text;
+        }
+
+        public void EnsureLoginPageIsOpen()
+        {
+            //Open the Swag Labs page in the browser if not opened yet
+            if (_webDriver.Url != SwagLabsLoginUrl)
+            {
+                _webDriver.Url = SwagLabsLoginUrl;
+            }
+            else
+            {
+                _webDriver.Close();
+                _webDriver.Url = SwagLabsLoginUrl;
+            }
+        }
+    }
+}
---
+++ SwagLabsLoginTests/Steps/LoginStepDefinitions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using SwagLabsLoginTest.Drivers;
+using NUnit.Framework;
+using SwagLabsLoginTest.PageObjects;
+
+namespace SwagLabsLoginTests.Steps
+{
+    [Binding]
+    public sealed class LoginStepDefinitions
+    {
+        //Page Objects needed for test
+        private readonly LoginPageObject _loginPageObject;
+        private readonly ProductsPageObject _productsPageObject;
+
+        public LoginStepDefinitions(BrowserDriver browserDriver)
+        {
+            _loginPageObject = new LoginPageObject(browserDriver.Current);
+            _productsPageObject = new ProductsPageObject(browserDriver.Current);
+        }
+
+        [Given(@"I am on the Sauce Demo Login Page")]
+        public void GivenIAmOnTheSauceDemoLoginPage()
+        {
+            _loginPageObject.EnsureLoginPageIsOpen();
+            Assert.IsTrue(_loginPageObject.LoginPageDisplayed(), "The login page is not displayed");
+        }
+
+        [When(@"I fill the account information for account StandardUser \(""(.*)"", ""(.*)""\) into the Username field and the Password field")]
+        public void WhenIFillTheAccountInformationForAccountStandardUserIntoTheUsernameFieldAndThePasswordField(string username, string password)
+        {
+            _loginPageObject.EnterUserName(username);
+            _loginPageObject.EnterPassword(password);
+        }
+
+        [When(@"I click the Login Button")]
+        public void WhenIClickTheLoginButton()
+        {
+            _loginPageObject.ClickLoginButton();
+        }
+
+        [Then(@"I am redirected to the Sauce Demo Main Page")]
+        public void ThenIAmRedirectedToTheSauceDemoMainPage()
+        {
+            _productsPageObject.EnsureProductsPageIsLoaded();
+        }
+
+        [Then(@"I verify the App Logo exists")]
+        public void ThenIVerifyTheAppLogoExists()
+        {
+            Assert.IsTrue(_productsPageObject.EnsureAppLogoIsDisplayed(), "The App Logo does not exist");
+        }
+
+        [When(@"I fill the account information for account LockedOutUser \(""(.*)"", ""(.*)""\) into the Username field and the Password field")]
+        public void WhenIFillTheAccountInformationForAccountLockedOutUserIntoTheUsernameFieldAndThePasswordField(string username, string password)
+        {
+            _loginPageObject.EnterUserName(username);
+            _loginPageObject.EnterPassword(password);
+        }
+
+        [Then(@"I verify the Error Message contains the text ""(.*)""")]
+        public void ThenIVerifyTheErrorMessageContainsTheText(string errorMessage)
+        {
+            Assert.AreEqual(_loginPageObject.CheckForLoginError(), errorMessage);
+        }
+
+    }
+}
